2fc7fd6b5230156357671cbfe9352c4e39f2f240
toge16 <2509105@upy.edu.mx>
2026-07-06 13:28:39 -0600

    Python portfolio

--- rango.py
+++
@@ -1,8 +0,0 @@
-#validar un numero dentro de un rango 
-n= int (input("Enter a number: "))
-rango =10 <= n <= 30
-print(rango)
-
-letra= input ("ingresar una letra: ")
-vocal = letra.lower() in "aeiou" #upper para mayusculas y lower para minusculas
-print(vocal)
