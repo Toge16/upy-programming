991b33bfc2c24dcbb504047fbc67c5e0d2207630
toge16 <2509105@upy.edu.mx>
2026-07-10 08:09:53 -0600

    Archivos

--- exercise13.py
+++
@@ -1,6 +0,0 @@
-n=int(input("ingresa un numero: "))
-res=0
-for i in range (2,n+1,2):
-    res=res+i
-    print(i)
-print("suma de pares", res)
--- exercise18.py
+++
@@ -1,7 +0,0 @@
-n=int(input("ingresa un numero: "))
-for r in range (1,n+1):
-    linea=""
-    for c in range (1, n+1):
-        linea=linea+ str(r*c) +  "\t"
-        print(linea)
-    
